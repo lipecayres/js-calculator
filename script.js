0066b28ff52300909a4b94b39258be53c7e23c4f
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-09-30 09:57:37 -0300

    buttons selection

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// Buttons Selection
+const numberButtons =document.querySelectorAll('[data-number]')
+const operationButtons =document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-allClear]')
+const previousButtonTextElement = document.querySelector('[data-previous-operand]')
+const currentButtonTextElement = document.querySelector('[data-current-operand]')
